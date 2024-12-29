d50322409af5f4e5fa30cc839871eeb46121fdeb
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-29 19:57:47 +0600

    Chef Recommended section compleate.

--- src/pages/Home/chefRecommends/ChefRecommends.jsx
+++ src/pages/Home/chefRecommends/ChefRecommends.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle";
+import RecommendedItemCard from "../../shared/recommendedItem/RecommendedItemCard";
 
 const ChefRecommends = () => {
+    const [recommendedItems, setRecommendedItems] = useState([]);
+    console.log(recommendedItems);
+    useEffect(() => {
+        fetch("/public/menu.json")
+            .then((res) => res.json())
+            .then((data) => {
+                setRecommendedItems(
+                    data.filter((item) => item.category === "offered")
+                );
+            });
+    }, []);
     return (
-        <div>
+        <div className="my-10">
             <SectionTitle
                 title={"Chef Recommends"}
                 subTitle={"Should Try"}
             ></SectionTitle>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                {
+                    recommendedItems.map((item) => <RecommendedItemCard key={item.id} item={item}></RecommendedItemCard>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/pages/shared/recommendedItem/RecommendedItemCard.jsx
@@ -0,0 +1,23 @@
+const RecommendedItemCard = ({ item }) => {
+    const { name, recipe, image } = item;
+    return (
+        <div className="bg-[#f1f1f1]">
+            <div>
+                <img className="w-full h-[250px]" src={image} alt="" />
+            </div>
+            <div className="p-4">
+                <div className="text-center my-3">
+                    <h1 className="text-2xl font-bold mb-2">{name}</h1>
+                    <p className="font-semibold text-sm">{recipe}</p>
+                </div>
+                <div className="flex justify-center">
+                    <button className="btn rounded-none border-2 border-[#bc8408] text-[#bc8408]">
+                        ADD TO CART
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RecommendedItemCard;
