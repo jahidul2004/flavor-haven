b21e1a86693e94d25f8b60bc5e7d8e5d51986825
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-29 19:07:34 +0600

    Data load from json file and filter functionality added.

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle";
 
 const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+    console.log(menu);
+    useEffect(() => {
+        fetch("/public/menu.json")
+            .then((res) => res.json())
+            .then((data) => {
+                setMenu(data.filter((item) => item.category === "popular"));
+            });
+    }, []);
     return (
         <div>
             <SectionTitle
                 subTitle={"Check it out"}
                 title={"From Our Menu"}
             ></SectionTitle>
+
+            <div></div>
         </div>
     );
 };
