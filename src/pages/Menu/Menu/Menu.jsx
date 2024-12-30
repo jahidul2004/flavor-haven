93d7f05a066a24840a8f6b0920c21c8a1eebf327
jahidul2004 <islamjahiduljihad@gmail.com>
2024-12-30 22:30:25 +0600

    Todays offer section added.

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,7 +1,23 @@
 import { Helmet } from "react-helmet-async";
 import QuickMessage from "../../../components/quickMessage/QuickMessage";
+import SectionTitle from "../../../components/SectionTitle";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import MenuItemCard from "../../shared/menuItem/MenuItemCard";
 
 const Menu = () => {
+    const [menus, setMenus] = useState([]);
+    const [todaysOffer, setTodaysOffer] = useState([]);
+
+    useEffect(() => {
+        setTodaysOffer(menus.filter((item) => item.category === "popular"));
+    }, []);
+
+    useEffect(() => {
+        axios.get("/public/menu.json").then((response) => {
+            setMenus(response.data);
+        });
+    }, []);
     return (
         <div>
             <Helmet>
@@ -16,6 +32,28 @@ const Menu = () => {
                 imagePath={"./src/assets/home/chef-service.jpg"}
             ></QuickMessage>
             {/* Banner end */}
+
+            {/* Todays offer */}
+            <div className="mx-4 my-10">
+                <SectionTitle
+                    subTitle={"Don't Miss"}
+                    title={"Todays Offer"}
+                ></SectionTitle>
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+                    {todaysOffer.map((offer) => (
+                        <MenuItemCard
+                            key={offer._id}
+                            item={offer}
+                        ></MenuItemCard>
+                    ))}
+                </div>
+                <div className="flex justify-center items-center mt-6">
+                    <button className="btn rounded-none border-2 border-[#bc8408] text-[#bc8408]">
+                        Order Your Favorite Food
+                    </button>
+                </div>
+            </div>
+            {/* Todays offer end */}
         </div>
     );
 };
