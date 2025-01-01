07a9df705c204baad0fff3e3c7dc57f618e485ff
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-01 20:34:32 +0600

    Paralex added.

--- src/components/quickMessage/QuickMessage.jsx
+++ src/components/quickMessage/QuickMessage.jsx
@@ -1,20 +1,30 @@
+import { Parallax } from "react-parallax";
+
 const QuickMessage = ({ imagePath, title, description, dark }) => {
     return (
-        <div
-            className={`mb-10 bg-cover bg-center h-96 flex items-center justify-center`}
-            style={{ backgroundImage: `url(${imagePath})` }}
+        <Parallax
+            blur={5}
+            bgImage={imagePath}
+            bgImageAlt="the cat"
+            strength={200}
         >
             <div
-                className={`w-[90%] md:w-[60%] h-[80%] md:h-[60%] ${
-                    dark ? "bg-[#00000067] text-white" : "bg-white text-black"
-                } flex flex-col items-center justify-center p-4 text-center`}
+                className={`mb-10 bg-cover bg-center h-96 flex items-center justify-center`}
             >
-                <h1 className="uppercase text-2xl md:text-3xl font-bold mb-2">
-                    {title}
-                </h1>
-                <p>{description}</p>
+                <div
+                    className={`w-[90%] md:w-[60%] h-[80%] md:h-[60%] ${
+                        dark
+                            ? "bg-[#00000067] text-white"
+                            : "bg-white text-black"
+                    } flex flex-col items-center justify-center p-4 text-center`}
+                >
+                    <h1 className="uppercase text-2xl md:text-3xl font-bold mb-2">
+                        {title}
+                    </h1>
+                    <p>{description}</p>
+                </div>
             </div>
-        </div>
+        </Parallax>
     );
 };
 
