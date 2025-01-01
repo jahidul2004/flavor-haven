207ceba44a4de46273be96570a3d71b5a3761aaf
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-01 20:45:34 +0600

    Menu category component added.

---
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -0,0 +1,13 @@
+import MenuItemCard from "../../shared/menuItem/MenuItemCard";
+
+const MenuCategory = ({ data }) => {
+    return (
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+            {data.map((item) => (
+                <MenuItemCard key={item._id} item={item}></MenuItemCard>
+            ))}
+        </div>
+    );
+};
+
+export default MenuCategory;
