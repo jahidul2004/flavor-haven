6eb81f0d03e9b1c5f733b5f4594b418848c3f98d
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-03 18:44:49 +0600

    useMenu custom hook added.:

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -2,28 +2,34 @@ import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle";
 import MenuItemCard from "../../shared/menuItem/MenuItemCard";
 import { Link } from "react-router-dom";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
-    console.log(menu);
-    useEffect(() => {
-        fetch("/public/menu.json")
-            .then((res) => res.json())
-            .then((data) => {
-                setMenu(data.filter((item) => item.category === "popular"));
-            });
-    }, []);
+    // const [menu, setMenu] = useState([]);
+    // console.log(menu);
+    // useEffect(() => {
+    //     fetch("/public/menu.json")
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             setMenu(data.filter((item) => item.category === "popular"));
+    //         });
+    // }, []);
+
+    const [menu] = useMenu();
+
     return (
         <div className="my-10">
             <SectionTitle
-                subTitle={"Check it out"}
+                subTitle={"Popular Item"}
                 title={"From Our Menu"}
             ></SectionTitle>
 
             <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-                {menu.map((item) => (
-                    <MenuItemCard key={item._id} item={item}></MenuItemCard>
-                ))}
+                {menu
+                    .filter((menuItem) => menuItem.category === "popular")
+                    ?.map((item) => (
+                        <MenuItemCard key={item._id} item={item}></MenuItemCard>
+                    ))}
             </div>
 
             <div className="flex justify-center mt-8">
