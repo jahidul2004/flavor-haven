9d96b9d01b723146ae08ac0a9f17c919422e5a80
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-03 18:58:46 +0600

    Desert and featured menu section added dynamically.

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,28 +1,41 @@
 import { Helmet } from "react-helmet-async";
 import QuickMessage from "../../../components/quickMessage/QuickMessage";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+import useMenu from "../../../hooks/useMenu";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const deserts = menu.filter((menuItem) => menuItem.category === "dessert");
+    const pizzas = menu.filter((menuItem) => menuItem.category === "pizza");
+    const salads = menu.filter((menuItem) => menuItem.category === "salad");
+    const soups = menu.filter((menuItem) => menuItem.category === "soup");
+    const offered = menu.filter((menuItem) => menuItem.category === "offered");
     return (
         <div>
             <Helmet>
                 <title>Flavor Haven | Menu</title>
             </Helmet>
 
-            {/* Banner */}
+            {/* Todays Offer */}
             <QuickMessage
                 title={"Our Menu"}
                 description={"Would You Like to try a dish?"}
                 dark={true}
                 imagePath={"./src/assets/home/chef-service.jpg"}
             ></QuickMessage>
-            {/* Banner end */}
+            <MenuCategory data={offered}></MenuCategory>
+            {/* Todays Offer */}
 
-            {/* Todays offer */}
-            <PopularMenu></PopularMenu>
-            {/* Todays offer end */}
+            {/* Todays Offer */}
+            <QuickMessage
+                title={"Our Menu"}
+                description={"Would You Like to try a dish?"}
+                dark={true}
+                imagePath={"./src/assets/home/featured.jpg"}
+            ></QuickMessage>
+            <MenuCategory data={deserts}></MenuCategory>
+            {/* Todays Offer */}
         </div>
     );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -2,8 +2,8 @@ import MenuItemCard from "../../shared/menuItem/MenuItemCard";
 
 const MenuCategory = ({ data }) => {
     return (
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-            {data.map((item) => (
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 my-10 mx-5">
+            {data?.map((item) => (
                 <MenuItemCard key={item._id} item={item}></MenuItemCard>
             ))}
         </div>
