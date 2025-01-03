ccf7ee83752ef37f8a43ed80247d83d9d1700ba8
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-03 19:33:33 +0600

    Popular menu section in home page updated.

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -3,43 +3,20 @@ import SectionTitle from "../../../components/SectionTitle";
 import MenuItemCard from "../../shared/menuItem/MenuItemCard";
 import { Link } from "react-router-dom";
 import useMenu from "../../../hooks/useMenu";
+import MenuCategory from "../../Menu/MenuCategory/MenuCategory";
 
 const PopularMenu = () => {
-    // const [menu, setMenu] = useState([]);
-    // console.log(menu);
-    // useEffect(() => {
-    //     fetch("/public/menu.json")
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             setMenu(data.filter((item) => item.category === "popular"));
-    //         });
-    // }, []);
-
     const [menu] = useMenu();
-
+    const popularMenu = menu.filter((item) => item.category === "popular");
     return (
         <div className="my-10">
-            <SectionTitle
-                subTitle={"Popular Item"}
-                title={"From Our Menu"}
-            ></SectionTitle>
-
-            <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-                {menu
-                    .filter((menuItem) => menuItem.category === "popular")
-                    ?.map((item) => (
-                        <MenuItemCard key={item._id} item={item}></MenuItemCard>
-                    ))}
-            </div>
-
-            <div className="flex justify-center mt-8">
-                <Link
-                    className="btn rounded-none border-2 border-[#bc8408] text-[#bc8408]"
-                    to={"/"}
-                >
-                    VIEW ALL MENU
-                </Link>
-            </div>
+            <MenuCategory
+                title={"Popular Menu"}
+                description={"Would You Like to try a dish?"}
+                data={popularMenu}
+                imagePath={"./src/assets/home/chef-service.jpg"}
+                isDark={true}
+            ></MenuCategory>
         </div>
     );
 };
--- src/pages/Home/home/Home.jsx
+++ src/pages/Home/home/Home.jsx
@@ -26,16 +26,6 @@ const Home = () => {
                 <Category></Category>
                 {/* Category end */}
 
-                {/* Quick Message */}
-                <QuickMessage
-                    title={"Flavor Haven"}
-                    description={
-                        "Welcome to Flavor Gaven, where every bite tells a stor Our project aims to create a unique dining experience,blending delightful flavors with a warm and inviting ambiance."
-                    }
-                    imagePath={"./src/assets/home/chef-service.jpg"}
-                ></QuickMessage>
-                {/* Quick Message end */}
-
                 {/* Popular Menu */}
                 <PopularMenu></PopularMenu>
                 {/* Popular Menu end */}
