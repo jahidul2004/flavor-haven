4c0d703975be2d19c86320880f9912678d3e90aa
jahidul2004 <islamjahiduljihad@gmail.com>
2025-01-03 20:34:34 +0600

    Tabs added.

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -11,6 +11,29 @@ const Order = () => {
                 imagePath={"./src/assets/shop/banner2.jpg"}
             ></QuickMessage>
             {/* Quick Message end */}
+
+            {/* Tabs */}
+            <div
+                role="tablist"
+                className="tabs tabs-boxed w-full md:w-1/2 mx-auto my-5 border border-[#bc8408] rounded-lg text-[#bc8408] font-semibold"
+            >
+                <a role="tab" className="tab">
+                    SALAD
+                </a>
+                <a role="tab" className="tab tab-active">
+                    PIZZA
+                </a>
+                <a role="tab" className="tab">
+                    SOUPS
+                </a>
+                <a role="tab" className="tab">
+                    DESSERTS
+                </a>
+                <a role="tab" className="tab">
+                    DRINKS
+                </a>
+            </div>
+            {/* Tabs end */}
         </div>
     );
 };
